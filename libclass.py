7e10ebce9801e7df1a05764094e6b6136a0ae175
Suresh Mariappan <nanotechsuresh@gmail.com>
2024-12-26 08:11:57 +0530

    External library

---
+++ libclass.py
@@ -0,0 +1,21 @@
+#import math
+#import numpy as np
+
+#print("It's math! It has type {}".format(type(math)))
+#print(dir(math))
+#print("pi to 4 significant digits = {:.4}".format(math.pi))
+
+# Roll 10 dice
+#rolls = np.random.randint(low=1, high=6, size=10)
+#print(rolls)
+
+#xlist = [[1,2,3],[2,4,6],]
+# Create a 2-dimensional array
+x = np.asarray(xlist)
+#print(xlist)
+#print(x)
+#print(x[1,-1])
+#print("xlist = {}\nx =\n{}".format(xlist, x))
+
+
+
